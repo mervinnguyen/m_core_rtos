cc95f38d528194de66e177ab555ab8ae4b5c62e8
Mervin Nguyen <mervinnguyen123@gmail.com>
2026-03-31 03:03:50 -0700

    Initialize periodic scheduler v2 workflow

---
+++ GCC_STM32CubeIDE/8_PeriodicScheduler_v2/Inc/osKernel.h
@@ -0,0 +1,18 @@
+#ifndef __OSKERNEL_H__
+#define __OSKERNEL_H__
+#include <stdint.h>
+#include "stm32f4xx.h"
+
+#define PERIOD    100
+
+void osKernelStackInit(int i);
+void osKernelLaunch(uint32_t quanta);
+uint8_t osKernelAddThreads(void(*task0)(void), void(*task1)(void), void(*task2)(void));
+void osSchedulerLaunch(void);
+void osKernelInit(void);
+void osThreadYield(void);
+void task3(void);
+void osSchedulerRoundRobin(void);
+
+
+#endif
